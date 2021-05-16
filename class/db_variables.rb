21eb5e37978f800de9eb1611e0cc4d03d9da8eb1
Kamil Suchanek <kamilsuchanek95@gmail.com>
2021-05-16 10:37:47 +0200

    class MiniDb and commands for variables inside

--- class/db_variables.rb
+++ class/db_variables.rb
@@ -1,27 +1,30 @@
 class DbVariables
+  attr_accessor :variables_list
 
   def initialize
     @variables_list = Hash.new
   end
 
-  def set name, value
+  def set name, value, variables_list = @variables_list
     # TODO: Exception/Error handling
-    @variables_list[name.to_s.delete(" \t\r\n").to_sym]  = value.to_s.delete(" \t\r\n")  if  validate_name and validate_value
+    if  validate_name(name) and validate_value(value)
+      variables_list[name.to_s.delete(" \t\r\n").to_sym] = value.to_s.delete(" \t\r\n")
+    end
   end
 
-  def get name
+  def get name, variables_list = @variables_list
     # TODO: If name doesnt exist? => nil, "NULL"
-    @variables_list[name.to_sym] if validate_name
+    variables_list[name.to_sym] if validate_name name
   end
   
-  def delete name
+  def delete name, variables_list = @variables_list
     # If that key doesnt exist => nil
-    @variables_list.delete(name.to_sym) if validate_name
+    variables_list.delete(name.to_sym) if validate_name name
   end
 
-  def count value
+  def count value, variables_list = @variables_list
     # count or 0, fine.
-    @variables_list.values.count(value) if validate_value
+    variables_list.values.count(value) if validate_value value
   end
 
 
--- class/mini_db.rb
+++ class/mini_db.rb
@@ -1,28 +1,72 @@
 require_relative 'db_variables'
+require 'set'
 
-class MiniDb
+class MiniDb < DbVariables
+  attr_accessor :db_variables, :name
 
   @@current_db_name  = ""
   @@databases_names = Set.new
   @@databases       = Array.new
 
-  def def initialize(db_name)
+  def initialize(db_name)
     # TODO: check if name is in use and add postfix with number eventually
     @@databases_names <<  db_name # save name to unique list
-    @db_variables     = DbVariables.new # create list variables
+
     @@databases       <<  self # add reference to object inside class
     @@current_db_name << db_name # active db workflow
-    @name             = db_name
+
+    @name             = db_name # name of database
+    @db_variables     = DbVariables.new # create list variables
+  end
+  
+  def set name, value, variables_list = self.db_variables.variables_list
+    super
   end
 
+  def get name, variables_list = self.db_variables.variables_list
+    super
+  end
+  
+  def delete name, variables_list = self.db_variables.variables_list
+    super
+  end
+
+  def count value, variables_list = self.db_variables.variables_list
+    super
+  end
+
+  # return reference to database
   def select db_name
-    @@databases.select {|db| db.name == db_name}
+    if database_exists? 
+      @@current_db_name = db_name
+      @@databases.select {|db| db.name == db_name}
+    end
+  end
+
+  def destroy db_name
+    if database_exists?
+      @@databases.delete {|db| db.name == db_name}
+      @@current_db_name = "" if @@current_db_name == db_name
+      @databases_names.delete db_name
+    end
+  end
+
+  def self.list
+    @@databases_names
+  end
 
   def self.all
-    @databases.inspect
+    @@databases.inspect
   end
 
   def self.count
     @@databases.count
   end
+
+  private
+
+  def database_exists? name
+    @@databases.include?(name)
+  end
+
 end
